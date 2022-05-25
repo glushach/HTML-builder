7a7bc7a94ebd38b7d1b312e6ac4e5d6c131fd1e4
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-05-25 21:43:47 +0300

    fix: task-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,10 +3,10 @@ const PATHBefore = path.join(__dirname, 'files');
 const PATHAfter = path.join(__dirname, 'files-copy');
 const fsPromises = require('fs').promises;
 
-function copyDir() {
-  fsPromises.mkdir(PATHAfter, {recursive: true}); // создание директории
+async function copyDir() {
+  await fsPromises.mkdir(PATHAfter, {recursive: true}); // создание директории
 
-  fsPromises.readdir(PATHAfter, (err) => {
+  await fsPromises.readdir(PATHAfter, (err) => {
     if (err) throw err; // очистка files-copy директории, если в директории files удалили файл
   }).then(files => {
     files.forEach(file => {
@@ -16,8 +16,9 @@ function copyDir() {
     });
   });
 
-  const files = fsPromises.readdir(PATHBefore); // добавление файлов в диреторию
-  files.then(files => {
+  await fsPromises.readdir(PATHBefore, (err) => {
+    if (err) throw err;
+  }).then(files => {
     files.forEach(file => {
       fsPromises.copyFile(path.join(PATHBefore, file), path.join(PATHAfter, file));
     });
